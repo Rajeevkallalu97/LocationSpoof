a26e0e4f6cfb4e8fc41d71f7ae35dcecc52bdaa6
Rajeevkallalu97 <Rajeev.Kallalu@exultglobal.com>
2024-07-06 20:48:28 +1000

    Initial

---
+++ locationspoof/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  locationspoof
+//
+//  Created by sai rajeev on 27/6/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Creadted by Rajeev")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ locationspoof/locationspoofApp.swift
@@ -0,0 +1,17 @@
+//
+//  locationspoofApp.swift
+//  locationspoof
+//
+//  Created by sai rajeev on 27/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct locationspoofApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
